d54d4e05299eaf9a00fd2998f9facfeda1b857b7
iceWallow3 <102610476+iceWallow3@users.noreply.github.com>
2022-07-06 16:23:48 +0300

    Add files via upload

--- scripts/script.js
+++ scripts/script.js
@@ -1,23 +1,23 @@
-const editButton = document.querySelector('.profile__editButton')
+const editButton = document.querySelector('.profile__edit-button') 
 const popup = document.querySelector('.popup')
 const closePopupButton = document.querySelector('.popup__close')
 
-const titleElement = document.querySelector('.profile-info__title')
-const nameFieldElement = document.querySelector('.popup__input_name')
+const titleElement = document.querySelector('.profile__info_title')
+const nameFieldElement = document.querySelector('.popup-input-name')
 
- const subtitleElement = document.querySelector('.profile-info__subtitle')
- const surnameFieldElement = document.querySelector('.popup__input_surname')
+ const subtitleElement = document.querySelector('.profile__info_subtitle')
+ const surnameFieldElement = document.querySelector('.popup-input-surname')
 
  const formElement = document.querySelector('.popup__form')
 
 //  console.log(closePopupButton)
 
 function openPopup(popupElement) {
-  popupElement.classList.add('popup__opened')
+  popupElement.classList.add('popup-opened')
 }
 
 function closePopup(popupElement) {
-    popupElement.classList.remove('popup__opened')
+    popupElement.classList.remove('popup-opened')
 }
 
 editButton.addEventListener('click', function() {
