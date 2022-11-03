4ba1693dd2a88d9774296e4209f901279aabe1e9
IceWallow3 <Geeo043@mail.ru>
2022-11-03 11:11:13 +0300

    осталась последняя проблема

---
+++ test/scripts/script.js
@@ -0,0 +1,17 @@
+let mashonka = ["говно", "залупа", "пенис", "хер"];
+for (let i = 0; i < mashonka.length; i = i + 1) {
+  console.log(mashonka[i]);
+}
+let morningList = [
+  "Встать с кровати",
+  "Почистить зубы",
+  "Сделать зарядку",
+  "Позавтракать",
+  "Помыть посуду",
+];
+for (let i = 0; i < morningList.length; i = i + 1) {
+  console.log(morningList[i]);
+}
+
+let qwerty;
+console.log(qwerty);
